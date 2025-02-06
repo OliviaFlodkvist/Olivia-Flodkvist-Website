9e46fdc05a8688d2a6caef9af593e37af9dbc09c
OliviaFlodkvist <olivia.flodkvist@live.se>
2025-02-06 16:11:27 +0100

    Resolved merge conflicts and made necessary updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // Update the pattern to include HTML and JS files
-    "./src/**/*.js", // This pattern now correctly matches all JavaScript files
+    "./*.html",                // Matches HTML files in the root directory
+    "./src/**/*.{html,js,css}",// Matches all HTML and JS files in src, includes CSS
   ],
   theme: {
     extend: {
       colors: {
-      "pinkk": "#f27a93",
+        "pinkk": "#f27a93",
       }
     },
   },
   plugins: [],
-}
+};
 
