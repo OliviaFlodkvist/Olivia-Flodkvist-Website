24d1c56bb457b2fa0a1ff0040668c8fb384fba78
OliviaFlodkvist <olivia.flodkvist@live.se>
2025-02-09 19:18:10 +0100

    Responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",  // Adjust this path to your HTML files in the public folder
+    "./public/**/*.js",  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
